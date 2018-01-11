c48093d020d53678ef56af894a54afd6bce51189
Li-Weijian <352642663@qq.com>
2018-01-11 23:36:36 +0800

    添加struts2第一天和练习代码

---
+++ Struts2_01/src/com/liweijian/hello/HelloAction.java
@@ -0,0 +1,16 @@
+package com.liweijian.hello;
+
+/**
+ * @Author:Liweijian
+ * @Description:
+ * @Date:Create in 22:49 2018/1/11 0011
+ */
+public class HelloAction {
+
+    public String hello(){
+
+        System.out.println("hello struts2");
+
+        return "abc";
+    }
+}
---
+++ Struts2_crm/src/com/liweijian/web/action/CustomerAction.java
@@ -0,0 +1,43 @@
+package com.liweijian.web.action;
+
+import com.liweijian.domain.Customer;
+import com.liweijian.service.CustomerService;
+import com.liweijian.service.impl.CustomerServiceImpl;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
+/**
+ * @Author:Liweijian
+ * @Description:
+ * @Date:Create in 23:23 2018/1/11 0011
+ */
+public class CustomerAction extends ActionSupport {
+
+    private CustomerService service = new CustomerServiceImpl();
+
+    public String list() throws Exception{
+
+        //1.获得参数
+        String cust_name = ServletActionContext.getRequest().getParameter("cust_name");
+        //2.新建离线查询
+        DetachedCriteria dc = DetachedCriteria.forClass(Customer.class);
+        //3.判断参数是否为空并组合条件
+        if (StringUtils.isNotBlank(cust_name)){
+            dc.add(Restrictions.like("cust_name","%"+cust_name+"%"));
+        }
+        //4.传递离线查询
+        List<Customer> list = service.customerList(dc);
+        //5.设置参数
+        ServletActionContext.getRequest().setAttribute("list",list);
+
+        return "list";
+    }
+
+
+
+}
