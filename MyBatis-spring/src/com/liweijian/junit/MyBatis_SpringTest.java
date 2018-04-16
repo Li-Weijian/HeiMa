a97da518f678a251969ba6a7614655744da756ac
Li-Weijian <352642663@qq.com>
2018-04-16 22:40:18 +0800

    mybatis整合spring使用Mapper动态代理开发

--- MyBatis-spring/src/com/liweijian/junit/MyBatis_SpringTest.java
+++ MyBatis-spring/src/com/liweijian/junit/MyBatis_SpringTest.java
@@ -1,6 +1,7 @@
 package com.liweijian.junit;
 
 import com.liweijian.dao.Userdao;
+import com.liweijian.mapper.UserMapper;
 import com.liweijian.pojo.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +14,14 @@ public class MyBatis_SpringTest {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         Userdao userDao = (Userdao) applicationContext.getBean("userDao");
         User user = userDao.selectUserById("1");
+        System.out.println(user);
+    }
+
+    @Test
+    public void findUserById(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserMapper userMapper = applicationContext.getBean(UserMapper.class);
+        User user = userMapper.findUserById("22");
         System.out.println(user);
     }
 }
---
+++ MyBatis-spring/src/com/liweijian/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.liweijian.mapper;
+
+import com.liweijian.pojo.User;
+
+public interface UserMapper {
+
+    public User findUserById(String id);
+}
